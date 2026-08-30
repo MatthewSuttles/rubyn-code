2bd3a3e5bf3ec21dfa50125bfc27e43bef131456
matthewsuttles <matthewsuttles13@yahoo.com>
2026-08-30 08:49:05 -0500

    Make engine release checks portable

--- spec/rubyn_code/auth/provider_keychain_spec.rb
+++ spec/rubyn_code/auth/provider_keychain_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe RubynCode::Auth::ProviderKeychain do
 
   it 'uses the native framework rather than a credential-bearing command' do
     expect(described_class.const_defined?(:SECURITY_EXECUTABLE, false)).to be(false)
-    expect(described_class.const_defined?(:Native, false)).to be(true)
+    expect(described_class.const_defined?(:Native, false)).to be(RUBY_PLATFORM.include?('darwin'))
   end
 
   it 'treats a missing credential as an ordinary absence' do
@@ -19,6 +19,8 @@ RSpec.describe RubynCode::Auth::ProviderKeychain do
   end
 
   it 'fails closed when Keychain revocation fails' do
+    skip 'macOS Security.framework is unavailable on this platform' unless RUBY_PLATFORM.include?('darwin')
+
     item = instance_double(Fiddle::Pointer)
     allow(described_class).to receive(:find_item).with('groq').and_return([described_class::SUCCESS, item])
     allow(described_class::Native).to receive(:SecKeychainItemDelete).with(item).and_return(-1)
